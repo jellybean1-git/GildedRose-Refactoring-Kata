20d24c7898820e79152c0f75e2b37d18af294d8e
Agnes Kijewski-Potgieter <agnes.kijewski.potgieter@ximedes.com>
2025-09-21 19:14:37 +0200

    Add test for aged brie

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,30 @@
 package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
+    @ParameterizedTest
+    @CsvSource(
+        "Aged Brie, 10, 0, 2, 2, 8",
+        "Aged Brie, 15, 40, 10, 50, 5",
+        "Aged Brie, 5, 45, 10, 50, -5",
+        "Aged Brie, 5, 20, 10, 35, -5"
+    )
+    fun `test update aged brie`(name: String, sellIn: Int, quality: Int, days: Int, qualityResult: Int, selInResult: Int) {
+        val items = listOf(Item(name, sellIn, quality))
         val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        (1..days).map {
+            app.updateQuality()
+        }
+
+        assertEquals(name, app.items[0].name)
+        assertEquals(qualityResult, app.items[0].quality)
+        assertEquals(selInResult, app.items[0].sellIn)
     }
-
 }
 
 
