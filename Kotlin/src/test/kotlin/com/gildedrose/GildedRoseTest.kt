989a76761262cf69c42b9c59535248f0f9a7aa43
Agnes Kijewski-Potgieter <agnes.kijewski.potgieter@ximedes.com>
2025-09-22 15:46:45 +0200

    Add Sulfuras and Backstage passes to the unit tests

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -11,9 +11,17 @@ internal class GildedRoseTest {
         "Aged Brie, 10, 0, 2, 2, 8",
         "Aged Brie, 15, 40, 10, 50, 5",
         "Aged Brie, 5, 45, 10, 50, -5",
-        "Aged Brie, 5, 20, 10, 35, -5"
+        "Aged Brie, 5, 20, 10, 35, -5",
+        "'Sulfuras, Hand of Ragnaros', 2, 80, 30, 80, 2",
+        "'Sulfuras, Hand of Ragnaros', 5, 80, 10, 80, 5",
+        "'Sulfuras, Hand of Ragnaros', -1, 80, 5, 80, -1",
+        "Backstage passes to a TAFKAL80ETC concert, 20, 5, 10, 15, 10",
+        "Backstage passes to a TAFKAL80ETC concert, 15, 30, 7, 39, 8",
+        "Backstage passes to a TAFKAL80ETC concert, 10, 35, 4, 43, 6",
+        "Backstage passes to a TAFKAL80ETC concert, 5, 35, 4, 47, 1",
+        "Backstage passes to a TAFKAL80ETC concert, 5, 48, 4, 50, 1"
     )
-    fun `test update aged brie`(name: String, sellIn: Int, quality: Int, days: Int, qualityResult: Int, selInResult: Int) {
+    fun `test updateQuality`(name: String, sellIn: Int, quality: Int, days: Int, qualityResult: Int, selInResult: Int) {
         val items = listOf(Item(name, sellIn, quality))
         val app = GildedRose(items)
 
