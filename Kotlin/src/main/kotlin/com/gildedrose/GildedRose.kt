d1a0f89cfb6c172cafdecdcfdd7a2f1893d17639
Agnes Kijewski-Potgieter <agnes.kijewski.potgieter@ximedes.com>
2025-09-22 16:17:29 +0200

    Add enum for Item name

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,13 +1,17 @@
 package com.gildedrose
 
+import com.gildedrose.ItemName.AGED_BRIE
+import com.gildedrose.ItemName.BACKSTAGE_PASSES
+import com.gildedrose.ItemName.SULFURAS
+
 const val MAX_QUALITY = 50
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != AGED_BRIE.label && items[i].name != BACKSTAGE_PASSES.label) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i].name != SULFURAS.label) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -15,7 +19,7 @@ class GildedRose(val items: List<Item>) {
                 if (items[i].quality < MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].name == BACKSTAGE_PASSES.label) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1
@@ -31,15 +35,15 @@ class GildedRose(val items: List<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != SULFURAS.label) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != AGED_BRIE.label) {
+                    if (items[i].name != BACKSTAGE_PASSES.label) {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i].name != SULFURAS.label) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
---
+++ Kotlin/src/main/kotlin/com/gildedrose/ItemName.kt
@@ -0,0 +1,7 @@
+package com.gildedrose
+
+enum class ItemName(val label: String) {
+    AGED_BRIE(label = "Aged Brie"),
+    BACKSTAGE_PASSES(label = "Backstage passes to a TAFKAL80ETC concert"),
+    SULFURAS(label = "Sulfuras, Hand of Ragnaros"),
+}
